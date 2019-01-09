e988c86c7c18e185a9f45c454f205a1b5363982f
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-01-09 22:12:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,6 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turns = turn_count(board)
-  turn_count(board) = turns =~ Even ? "X" : "O"
+  turns = turn_count(board) =~ Even ? "X" : "O"
 end
